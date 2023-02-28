09fa46312b7cff9e31527e47970cac278501adfb
Ruslanios22 <ilgamovich@gmail.com>
2023-02-28 16:59:12 +0800

    App Icon and Login View

--- ydisk_skillbox/SkillboxDrive/SkillboxDrive/Login/LoginViewController.swift
+++ ydisk_skillbox/SkillboxDrive/SkillboxDrive/Login/LoginViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func buttonPressed() {
+        
     }
 }
 
