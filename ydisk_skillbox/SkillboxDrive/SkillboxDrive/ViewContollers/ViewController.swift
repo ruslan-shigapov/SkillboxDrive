51b102b252bbef3e66cf54f7d2f86ba8eb1e6d6c
Ruslanios22 <ilgamovich@gmail.com>
2023-02-28 11:44:01 +0800

    Setup project

---
+++ ydisk_skillbox/SkillboxDrive/SkillboxDrive/ViewContollers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SkillboxDrive
+//
+//  Created by Руслан Шигапов on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
