2ce0a884132b7858162218a38969b872bd6154ee
Ruslanios22 <ilgamovich@gmail.com>
2023-02-28 12:48:20 +0800

    Launch Screen

--- ydisk_skillbox/SkillboxDrive/SkillboxDrive/ViewContollers/LoginViewController.swift
+++ ydisk_skillbox/SkillboxDrive/SkillboxDrive/ViewContollers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  SkillboxDrive
 //
 //  Created by Руслан Шигапов on 28.02.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
