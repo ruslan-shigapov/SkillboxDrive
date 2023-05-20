804985fc56b3ef946429784fa60bf0735d09531e
Ruslanios22 <ilgamovich@gmail.com>
2023-05-20 15:18:23 +0300

    Log out

--- ydisk_skillbox/SkillboxDrive/SkillboxDrive/Profile/ProfileViewController.swift
+++ ydisk_skillbox/SkillboxDrive/SkillboxDrive/Profile/ProfileViewController.swift
@@ -6,29 +6,86 @@
 //
 
 import UIKit
+import ALProgressView
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var progressView: UIView!
     @IBOutlet var publishedFilesButton: UIView!
     @IBOutlet var circleViews: [UIView]!
     
+    @IBOutlet var capacityLabel: UILabel!
     @IBOutlet var usedMemoryLabel: UILabel!
     @IBOutlet var availableMemoryLabel: UILabel!
     
+    private lazy var progressRing = ALProgressRing()
+    private let capacity = 20
+    private let usedMemory = 4
+    private let availableMemory = 16
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        setupViews()
+        setupUI()
+        setupConstraints()
+        setupProgressRing()
     }
     
-    private func setupViews() {
+    @IBAction func exitButtonPressed(_ sender: UIBarButtonItem) {
+        showLogOutAlert()
+    }
+    
+    // Fetch data
+    
+    private func setupUI() {
+        progressView.addSubview(progressRing)
         publishedFilesButton.layer.cornerRadius = 10
         publishedFilesButton.layer.shadowOpacity = 0.3
         publishedFilesButton.layer.shadowColor = UIColor.black.cgColor
         publishedFilesButton.layer.shadowRadius = 12
         circleViews.forEach { $0.layer.cornerRadius = $0.frame.width / 2 }
+        capacityLabel.text = "\(capacity) GB"
+        usedMemoryLabel.text = "\(usedMemory) GB - used"
+        availableMemoryLabel.text = "\(availableMemory) GB - available"
     }
     
-    @IBAction func logOutButton(_ sender: UIBarButtonItem) {
-        
+    private func setupConstraints() {
+        progressRing.translatesAutoresizingMaskIntoConstraints = false
+        progressRing.centerXAnchor.constraint(equalTo: progressView.centerXAnchor).isActive = true
+        progressRing.centerYAnchor.constraint(equalTo: progressView.centerYAnchor).isActive = true
+        progressRing.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        progressRing.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    private func setupProgressRing() {
+        let progress = Float(usedMemory) / Float(capacity)
+        progressRing.setProgress(progress, animated: true)
+        progressRing.startColor = circleViews[0].backgroundColor ?? .systemPink
+        progressRing.endColor = circleViews[0].backgroundColor ?? .systemPink
+        progressRing.grooveColor = circleViews[1].backgroundColor ?? .systemGray
+    }
+    
+    // MARK: - Alert Controllers
+    private func showLogOutAlert() {
+        let alert = UIAlertController(title: "Profile", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let logOutAction = UIAlertAction(title: "Log Out", style: .destructive) { [unowned self] _ in
+            showExitAlert()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(logOutAction)
+        present(alert, animated: true)
+    }
+    
+    private func showExitAlert() {
+        let alert = UIAlertController(title: "Exit", message: "Are you sure?", preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "Yes", style: .cancel) { _ in
+            
+            // Action
+            
+        }
+        let noAction = UIAlertAction(title: "No", style: .destructive)
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        present(alert, animated: true)
     }
 }
