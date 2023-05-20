452760d1516176ee75cb155d3b8eb394ae22b4b9
Ruslanios22 <ilgamovich@gmail.com>
2023-05-20 14:13:18 +0300

    Profile View

--- ydisk_skillbox/SkillboxDrive/SkillboxDrive/Profile/ProfileViewController.swift
+++ ydisk_skillbox/SkillboxDrive/SkillboxDrive/Profile/ProfileViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var publishedFilesButton: UIView!
+    @IBOutlet var circleViews: [UIView]!
+    
+    @IBOutlet var usedMemoryLabel: UILabel!
+    @IBOutlet var availableMemoryLabel: UILabel!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        publishedFilesButton.layer.cornerRadius = 10
+        publishedFilesButton.layer.shadowOpacity = 0.3
+        publishedFilesButton.layer.shadowColor = UIColor.black.cgColor
+        publishedFilesButton.layer.shadowRadius = 12
+        circleViews.forEach { $0.layer.cornerRadius = $0.frame.width / 2 }
+    }
+    
+    @IBAction func logOutButton(_ sender: UIBarButtonItem) {
         
     }
 }
